4395c9b898f508888e6877f8697602db6426b18c
Sendiko <sarangtawon897@gmail.com>
2023-02-04 08:17:28 +0700

    initial commit

---
+++ Inventory App - LKS/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Inventory_App___LKS
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.store_label = new System.Windows.Forms.Label();
+            this.productTable = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.productLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.textBoxBarang = new System.Windows.Forms.TextBox();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.productTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // store_label
+            // 
+            this.store_label.AutoSize = true;
+            this.store_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.store_label.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.store_label.Location = new System.Drawing.Point(486, 9);
+            this.store_label.Name = "store_label";
+            this.store_label.Size = new System.Drawing.Size(211, 32);
+            this.store_label.TabIndex = 0;
+            this.store_label.Text = "Sendiko\'s Shop";
+            this.store_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // productTable
+            // 
+            this.productTable.AllowUserToAddRows = false;
+            this.productTable.AllowUserToDeleteRows = false;
+            this.productTable.AllowUserToResizeColumns = false;
+            this.productTable.AllowUserToResizeRows = false;
+            this.productTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.productTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.productTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.productTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productTable.Location = new System.Drawing.Point(12, 44);
+            this.productTable.Name = "productTable";
+            this.productTable.ReadOnly = true;
+            this.productTable.RowHeadersWidth = 51;
+            this.productTable.RowTemplate.Height = 24;
+            this.productTable.Size = new System.Drawing.Size(1083, 241);
+            this.productTable.TabIndex = 1;
+            this.productTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.productTable_CellContentClick);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(17, 365);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(383, 43);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(406, 365);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(349, 43);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonDelete.Location = new System.Drawing.Point(761, 365);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(331, 43);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // productLabel
+            // 
+            this.productLabel.AutoSize = true;
+            this.productLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.productLabel.Location = new System.Drawing.Point(182, 300);
+            this.productLabel.Name = "productLabel";
+            this.productLabel.Size = new System.Drawing.Size(130, 25);
+            this.productLabel.TabIndex = 5;
+            this.productLabel.Text = "Nama barang";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.categoryLabel.Location = new System.Drawing.Point(526, 300);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(85, 25);
+            this.categoryLabel.TabIndex = 6;
+            this.categoryLabel.Text = "Kategori";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.priceLabel.Location = new System.Drawing.Point(838, 301);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(65, 25);
+            this.priceLabel.TabIndex = 7;
+            this.priceLabel.Text = "Harga";
+            // 
+            // textBoxBarang
+            // 
+            this.textBoxBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBarang.Location = new System.Drawing.Point(187, 329);
+            this.textBoxBarang.Name = "textBoxBarang";
+            this.textBoxBarang.Size = new System.Drawing.Size(338, 30);
+            this.textBoxBarang.TabIndex = 8;
+            this.textBoxBarang.TextChanged += new System.EventHandler(this.textBoxBarang_TextChanged);
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCategory.Location = new System.Drawing.Point(531, 329);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(306, 30);
+            this.textBoxCategory.TabIndex = 9;
+            this.textBoxCategory.TextChanged += new System.EventHandler(this.textBoxCategory_TextChanged);
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPrice.Location = new System.Drawing.Point(843, 329);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(249, 30);
+            this.textBoxPrice.TabIndex = 10;
+            this.textBoxPrice.TextChanged += new System.EventHandler(this.textBoxPrice_TextChanged);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxID.Location = new System.Drawing.Point(17, 329);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(164, 30);
+            this.textBoxID.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(12, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Id";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1003, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 37);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1104, 420);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxBarang);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.productLabel);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.productTable);
+            this.Controls.Add(this.store_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Inventory";
+            this.Text = "Inventory";
+            this.Load += new System.EventHandler(this.Inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label store_label;
+        private System.Windows.Forms.DataGridView productTable;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label productLabel;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox textBoxBarang;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Inventory App - LKS/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Inventory_App___LKS
+{
+    public partial class Inventory : Form
+    {
+
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+        private void Inventory_Load(object sender, EventArgs e)
+        {
+            var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+            connection.Open();
+            String readQuery = "SELECT * FROM product";
+            MySqlCommand command = new MySqlCommand(readQuery, connection); 
+            MySqlDataAdapter adapter = new MySqlDataAdapter(); 
+            adapter.SelectCommand= command;
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            productTable.DataSource = dataTable;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if(this.textBoxID.Text.Length == 0 && this.textBoxBarang.Text.Length == 0 && this.textBoxCategory.Text.Length == 0 && this.textBoxPrice.Text.Length == 0)
+            {
+                String title = "Data invaild";
+                String message = "The data is invalid, please check the data again";
+                MessageBox.Show(message, title);
+            } else
+            {
+                var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+                String insertQuery = "INSERT INTO product (Id, name, category, price) VALUES ('" + this.textBoxID.Text + "', '" + this.textBoxBarang.Text + "', '" + this.textBoxCategory.Text + "', '" + this.textBoxPrice.Text + "')";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+                this.textBoxID.Text = "";
+                this.textBoxBarang.Text = "";
+                this.textBoxCategory.Text = "";
+                this.textBoxPrice.Text = "";
+                connection.Close();
+                String title = "Query update";
+                String message = "Data successfully created";
+                DialogResult result = MessageBox.Show(message, title);
+                if (result == DialogResult.OK)
+                {
+                    connection.Open();
+                    String readQuery = "SELECT * FROM product";
+                    MySqlCommand commandRead = new MySqlCommand(readQuery, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = commandRead;
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    productTable.DataSource = dataTable;
+                    connection.Close();
+                }
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (this.textBoxID.Text.Length == 0 && this.textBoxBarang.Text.Length == 0 && this.textBoxCategory.Text.Length == 0 && this.textBoxPrice.Text.Length == 0)
+            {
+                String title = "Data invaild";
+                String message = "The data is invalid, please check the data again";
+                MessageBox.Show(message, title);
+            } else
+            { 
+                var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+                connection.Open();
+                String updateQuery = "UPDATE product SET name= '" + this.textBoxBarang.Text + "', category='" + this.textBoxCategory.Text + "', price='" + this.textBoxPrice.Text + "' WHERE Id='" + this.textBoxID.Text + "'";
+                MySqlCommand command = new MySqlCommand(updateQuery, connection);
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+                this.textBoxID.Text = "";
+                this.textBoxBarang.Text = "";
+                this.textBoxCategory.Text = "";
+                this.textBoxPrice.Text = "";
+                connection.Close();
+                String title = "Query update";
+                String message = "Data successfully updated";
+                DialogResult result = MessageBox.Show(message, title);
+                if (result == DialogResult.OK)
+                {
+                    connection.Open();
+                    String readQuery = "SELECT * FROM product";
+                    MySqlCommand commandRead = new MySqlCommand(readQuery, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = commandRead;
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    productTable.DataSource = dataTable;
+                    connection.Close();
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (this.textBoxID.Text.Length == 0)
+            {
+                String title = "Data invalid";
+                String message = "The data input is invalid";
+                DialogResult result = MessageBox.Show(message, title);
+                if (result == DialogResult.OK)
+                {
+                    var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+                    connection.Open();
+                    String readQuery = "SELECT * FROM product";
+                    MySqlCommand commandRead = new MySqlCommand(readQuery, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = commandRead;
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    productTable.DataSource = dataTable;
+                    connection.Close();
+                }
+            }
+            else
+            {
+                var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+                connection.Open();
+                String deleteQuery = "DELETE FROM product WHERE Id= '" + this.textBoxID.Text + "';";
+                MySqlCommand command = new MySqlCommand(deleteQuery, connection);
+                MySqlDataReader reader;
+                reader = command.ExecuteReader();
+                connection.Close();
+                this.textBoxID.Text = "";
+                String title = "Query update";
+                String message = "Data successfully updated";
+                DialogResult result = MessageBox.Show(message, title);
+                if (result == DialogResult.OK)
+                {
+                    connection.Open();
+                    String readQuery = "SELECT * FROM product";
+                    MySqlCommand commandRead = new MySqlCommand(readQuery, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = commandRead;
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    productTable.DataSource = dataTable;
+                    connection.Close();
+                }
+            }
+        }
+
+        private void productTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+            connection.Open();
+            String readQuery = "SELECT * FROM product";
+            MySqlCommand command = new MySqlCommand(readQuery, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            productTable.DataSource = dataTable;
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connection = new MySqlConnection("Server=127.0.0.1;Database=inventory;Uid=root;Pwd=;");
+            connection.Open();
+            String readQuery = "SELECT * FROM product";
+            MySqlCommand command = new MySqlCommand(readQuery, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            productTable.DataSource = dataTable;
+            connection.Close();
+        }
+
+        private void textBoxBarang_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxCategory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
